87172ce8d2df433927b55936b1777489316cdaaf
Felix Beuster <felix.beuster@beusterse.de>
2022-09-17 17:22:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+YouTube Stream Page
+===
+
+Just trying to improve the YouTUbe live stream watch page. We want the chat next to the video, and the video should take as much space as possible.
---
+++ main.js
@@ -0,0 +1,19 @@
+function addScript(filename) {
+  var script = document.createElement("script");
+  script.src = chrome.runtime.getURL(filename);
+  script.type = "text/javascript";
+  document.getElementsByTagName("body")[0].appendChild(script);
+}
+
+function addstylesheet(filename) {
+  var link = document.createElement("link");
+  link.href = chrome.runtime.getURL(filename);
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+if (window.location.href.match(/(https:\/\/(.*youtube\.com\/.*))/i)){
+  addScript("watchpage.js");
+  addstylesheet("watchpage.css");
+}
