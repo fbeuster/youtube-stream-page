4b0a58ad2217d283ec77efcb7308e0612084c8eb
Felix Beuster <felix.beuster@beusterse.de>
2022-09-21 20:18:14 +0200

    improved body class setting, fixes #4

--- js/watchpage.js
+++ js/watchpage.js
@@ -1,3 +1,5 @@
+var isWatchPage = false;
+
 function vh(percent) {
   var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
   return (percent * h) / 100;
@@ -9,44 +11,79 @@ function vw(percent) {
 }
 
 function setPlayerSize() {
-  var ytd_watch_flexy = document.querySelector('ytd-watch-flexy[flexy]');
-  var video_element = document.getElementById('movie_player').getElementsByTagName('video')[0];
-  var yt_styles = window.getComputedStyle(ytd_watch_flexy);
-
-  var chat_width = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-sidebar-width'), 10);
-  var masthead_height = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-masthead-height'), 10);
-  var margin_6x = parseInt(yt_styles.getPropertyValue('--ytd-margin-6x'), 10);
-  var space_under_video = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-space-below-player'), 10);
-
-  var available_width = vw(100) - chat_width - margin_6x;
-  var available_height = vh(100) - masthead_height - space_under_video - margin_6x;
-
-  var target_width = 0;
-  var target_height = 0;
-
-  var ratio_width = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-width-ratio'), 10);
-  var ratio_height = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-height-ratio'), 10);
-  var ratio = ratio_width / ratio_height;
-
-  if (available_width / ratio < available_height) {
-    target_width = available_width;
-    target_height = available_width / ratio;
+  if (!isWatchPage) {
+    document.body.style.removeProperty('--video-width');
+    document.body.style.removeProperty('--video-height');
 
   } else {
-    target_width = available_height * ratio;
-    target_height = available_height
-  }
+    var ytd_watch_flexy = document.querySelector('ytd-watch-flexy[flexy]');
+    var video_element = document.getElementById('movie_player').getElementsByTagName('video')[0];
+    var yt_styles = window.getComputedStyle(ytd_watch_flexy);
 
-  document.body.style.setProperty('--video-width', target_width + 'px');
-  document.body.style.setProperty('--video-height', target_height + 'px');
+    var chat_width = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-sidebar-width'), 10);
+    var masthead_height = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-masthead-height'), 10);
+    var margin_6x = parseInt(yt_styles.getPropertyValue('--ytd-margin-6x'), 10);
+    var space_under_video = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-space-below-player'), 10);
+
+    var available_width = vw(100) - chat_width - margin_6x;
+    var available_height = vh(100) - masthead_height - space_under_video - margin_6x;
+
+    var target_width = 0;
+    var target_height = 0;
+
+    var ratio_width = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-width-ratio'), 10);
+    var ratio_height = parseInt(yt_styles.getPropertyValue('--ytd-watch-flexy-height-ratio'), 10);
+    var ratio = ratio_width / ratio_height;
+
+    if (available_width / ratio < available_height) {
+      target_width = available_width;
+      target_height = available_width / ratio;
+
+    } else {
+      target_width = available_height * ratio;
+      target_height = available_height
+    }
+
+    document.body.style.setProperty('--video-width', target_width + 'px');
+    document.body.style.setProperty('--video-height', target_height + 'px');
+  }
 }
 
-setTimeout(() => {
-  if (document.getElementById('chat') != null) {
-    document.getElementsByTagName('body')[0].classList.add('yt-stream-page');
+function locationChanged(event) {
+  isWatchPage = event.detail.pageType === 'watch';
 
+  setTimeout(() => {
+    updateBodyClass();
     setPlayerSize();
+  }, 1000);
+}
 
-    window.addEventListener('resize', setPlayerSize, true);
-  }
-}, 2000);
+function updateBodyClass() {
+  var bodyClass = 'yt-stream-page';
+
+  setTimeout(() => {
+    if (isWatchPage) {
+      document.getElementsByTagName('body')[0].classList.add(bodyClass);
+
+    } else {
+      document.getElementsByTagName('body')[0].classList.remove(bodyClass);
+    }
+  }, 1000);
+}
+
+function init() {
+  // listen to location changed
+  window.addEventListener('yt-navigate-finish', locationChanged, true)
+
+  // listen to resizes
+  window.addEventListener('resize', setPlayerSize, true);
+
+  isWatchPage = document.location.pathname == '/watch';
+
+  setTimeout(() => {
+    updateBodyClass();
+    setPlayerSize();
+  }, 1000);
+}
+
+init();
